6c209769397aa0ba268ca0a8cde358a47c8e3f2d
Erocknine <echeungsk@gmail.com>
2020-07-10 13:12:51 -0400

    temporary spans for anchors in navbar.

--- src/containers/navbar.js
+++ src/containers/navbar.js
@@ -3,14 +3,14 @@ import React from 'react'
 export default function Navbar() {
   return(
     <nav className="navbar">
-      <a href="">About</a>
-      <a href="">Services</a>
+      <span>About</span>
+      <span>Services</span>
       <div className="hies-name">
         <img src="images/new-hies.png" alt="hies logo"/>
         <h1 className="hies">HIES</h1>
       </div>
-      <a href="">Projects</a>
-      <a href="">Contact</a>
+      <span>Projects</span>
+      <span>Contact</span>
     </nav>
   )
 }
