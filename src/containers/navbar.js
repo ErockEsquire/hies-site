2f3a1d0389594531fcd06fc07590ba14f4bafdee
Erocknine <echeungsk@gmail.com>
2020-07-18 17:34:04 -0400

    added dropdown to navbar

--- src/containers/navbar.js
+++ src/containers/navbar.js
@@ -1,16 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Navbar() {
+
+  const services = [{name: "Environmental Services"}, {name: "Tsunami Services"}, {name: "International Management"}];
+  const projects = [{name: "Environmental Projects"}, {name: "GIS Projects"}];
+
   return(
     <nav className="navbar">
-      <span>About</span>
-      <span>Services</span>
+      <NavitemLink>About</NavitemLink>
+      <NavitemLink>Contact</NavitemLink>
       <div className="hies-name">
         <img src="images/new-hies.png" alt="hies logo"/>
         <h1 className="hies">HIES</h1>
       </div>
-      <span>Projects</span>
-      <span>Contact</span>
+      <NavitemList list={projects}>Projects</NavitemList>
+      <NavitemList list={services}>Services</NavitemList>
     </nav>
+  )
+}
+
+const NavitemLink = (props) => {
+  return(
+    <div className="nav-item">
+      <span>
+        {props.children}
+      </span>
+    </div>
+  )
+}
+
+const NavitemList = (props) => {
+  const { list } = props
+  const [ open, setOpen ] = useState(false)
+  return(
+    <div className="nav-item" onMouseEnter={() => setOpen(true)} onMouseLeave={() => setOpen(false)} onMouseLeave={() => setOpen(false)}>
+      <span>
+        {props.children}
+      </span>
+      <Dropdown list={list} open={open}></Dropdown>
+    </div>
+  )
+}
+
+const Dropdown = ({ list, open }) => {
+  const renderList = () => {
+    return list.map(item => <li className="nav-dropdown-li">{item.name}</li>)
+  }
+
+  return(
+    <ul className={open ? "nav-dropdown-open":"nav-dropdown-closed"} >
+      {renderList()}
+    </ul>
   )
 }
