c776d5141aa71208df54105fa55c46ed1faf14ca
erocknine <erock.esquire@gmail.com>
2021-01-07 01:53:37 -0500

    removed unused variables

--- src/home/water.js
+++ src/home/water.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, Fragment } from 'react';
+import React, { useEffect, Fragment } from 'react';
 import '../style/water.scss'
 import ScrollAnimation from 'react-animate-on-scroll';
 import * as PIXI from 'pixi.js-legacy'
@@ -52,46 +52,46 @@ export default function Water() {
 //<script src="https://cdnjs.cloudflare.com/ajax/libs/pixi.js/5.1.3/pixi.min.js"></script> */}
 //install pixi.js
 
-const useScript = (ref) => {
-  useEffect(() => {
-    const script = document.createElement('script');
+// const useScript = (ref) => {
+//   useEffect(() => {
+//     const script = document.createElement('script');
 
-    script.setAttribute('type', 'text/javascript');
-    script.innerHTML = `function initPixi() {
-      app = new PIXI.Application({width: window.innerWidth, height: window.innerHeight});
-      const container = document.getElementById('water-script')
-      container.appendChild(app.view);
+//     script.setAttribute('type', 'text/javascript');
+//     script.innerHTML = `function initPixi() {
+//       app = new PIXI.Application({width: window.innerWidth, height: window.innerHeight});
+//       const container = document.getElementById('water-script')
+//       container.appendChild(app.view);
   
-      let image = new PIXI.Sprite.from("./images/bluewater.png");
-      image.width = window.innerWidth;
-      image.height = window.innerHeight;
-      app.stage.addChild(image);
+//       let image = new PIXI.Sprite.from("./images/bluewater.png");
+//       image.width = window.innerWidth;
+//       image.height = window.innerHeight;
+//       app.stage.addChild(image);
   
-      displacementSprite = new PIXI.Sprite.from("./images/cloud.jpg");
-      displacementFilter = new PIXI.filters.DisplacementFilter(displacementSprite);
-      displacementSprite.texture.baseTexture.wrapMode = PIXI.WRAP_MODES.REPEAT;
-      app.stage.addChild(displacementSprite);
-      app.stage.filters = [displacementFilter];
+//       displacementSprite = new PIXI.Sprite.from("./images/cloud.jpg");
+//       displacementFilter = new PIXI.filters.DisplacementFilter(displacementSprite);
+//       displacementSprite.texture.baseTexture.wrapMode = PIXI.WRAP_MODES.REPEAT;
+//       app.stage.addChild(displacementSprite);
+//       app.stage.filters = [displacementFilter];
   
-      app.renderer.view.style.transform = 'scale(1.00)';
+//       app.renderer.view.style.transform = 'scale(1.00)';
   
-      displacementSprite.scale.x = 4;
-      displacementSprite.scale.y = 4;
-      animate();
-    }
+//       displacementSprite.scale.x = 4;
+//       displacementSprite.scale.y = 4;
+//       animate();
+//     }
   
-    function animate() {
-      displacementSprite.x += 8;
-      displacementSprite.y += 3;
-      requestAnimationFrame(animate);
-    }
+//     function animate() {
+//       displacementSprite.x += 8;
+//       displacementSprite.y += 3;
+//       requestAnimationFrame(animate);
+//     }
   
-    initPixi();`;
+//     initPixi();`;
 
-    script.async = true;
+//     script.async = true;
 
-    ref.current.appendChild(script);
+//     ref.current.appendChild(script);
     
-  }, [ref])
+//   }, [ref])
 
-}
+// }
